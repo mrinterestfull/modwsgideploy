6eaec0ec66a7ac54272f7ce321639d9c78c81272
Lukasz Szybalski <szybalski@gmail.com>
2008-11-23 23:04:58 -0600

    Initial Import

---
+++ modwsgideploy/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='modwsgideploy',
+      version=version,
+      description="Template that helps to deploy via mod_wsgi and apache.",
+      long_description="""\
+Templates builds a wsgi file and apache config file that user puts in apache and runs the app in few simple steps.""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='apache mod_wsgi turbogears pylons deploy script',
+      author='Lukasz Szybalski',
+      author_email='szybalski@gmail.com',
+      url='http://lucasmanual.com/mywiki/modwsgideploy',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
