ea1dfbff7a6704d4edf1654e4998f362d4b3bae7
Lukasz Szybalski <szybalski@gmail.com>
2009-09-26 22:43:58 -0500

    Fixing readme and setup.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,17 +1,26 @@
-from setuptools import setup, find_packages
+#Gets setuptools
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
 import sys, os
 
-version = '0.4.17'
+version = '0.4.18'
+
+readme=long_description=open("README.txt").read()
 
 setup(name='modwsgideploy',
       version=version,
-      description="Deploy Turbogears2 or Pylons via apache and modwsgi.",
-      long_description=open("README.txt").read(),
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='apache mod_wsgi turbogears pylons deploy script',
       author='Lukasz Szybalski',
       author_email='szybalski@gmail.com',
       url='http://lucasmanual.com/mywiki/modwsgideploy',
+      description="Deploy Turbogears2 or Pylons via apache and modwsgi.",
+      long_description=readme,
       license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
