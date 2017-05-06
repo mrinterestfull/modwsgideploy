3cd23a45156676541cf55ed224f44e0b25d9d321
Lukasz Szybalski <szybalski@gmail.com>
2017-05-06 15:36:12 -0500

    Starting to modify templates for apache

--- modwsgideploy/hooks/pre_gen_project.py
+++ modwsgideploy/hooks/pre_gen_project.py
@@ -4,3 +4,22 @@ except NameError:
     choice = raw_input('You said Y to subdomain').lower()
 
 print(choice)
+
+import os
+#os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+workfolder=os.path.dirname(os.path.realpath(__file__))
+#workfolder=os.path.dirname(__file__)
+
+try:
+    choice2 = input('Is this the project location? Press Enter if yes, otherwise enter a new path: '+str(workfolder))
+    if choice2=='':
+        print(workfolder)
+    else:
+        print(choice2)
+except NameError:
+    choice2 = input('Is this the project location? Press Enter if yes, otherwise enter a new path: '+str(workfolder))
+    if choice2=='':
+        print(workfolder)
+    else:
+        print(choice2)
+#current_folder=os.path.dirname(os.path.realpath(__file__))
