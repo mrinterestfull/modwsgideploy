85e1e27e97e2aaa6868c5a0ef354b0a7c8c66668
Lukasz Szybalski <szybalski@gmail.com>
2017-05-28 21:52:49 -0500

    Got it workign with python setup.py install

--- modwsgideploy/modwsgideploy/cli.py
+++ modwsgideploy/modwsgideploy/cli.py
@@ -11,11 +11,11 @@ def main(args=None):
     # Create project from the cookiecutter-pypackage/ template
     import os
     from modwsgideploy import __file__ as modwsgideploy__file__
-    package_path=os.path.abspath(os.path.join(modwsgideploy__file__,'../../'))
+    package_path=os.path.abspath(os.path.join(modwsgideploy__file__,'../'))
     cookiecutter(package_path)
     click.echo("\nThank you for deploying with modwsgideploy"
                "\nBuild in Chicago, IL, USA")
-    click.echo('''\nIf you have an idea that will make other peoples' life better and you need a partner.\n Reach out to us: parnter@dataassistant.co''')
+    click.echo('''\nIf you have an idea that will make other people's' life better and you need a partner.\n Reach out to us: parnter@DataAssistant.CO\n''')
 
 
 if __name__ == "__main__":
--- modwsgideploy/setup.py
+++ modwsgideploy/setup.py
@@ -18,6 +18,22 @@ test_requirements = [
             ]
 
 
+#Needed to add to package_data
+def add_to_package_data(pkg, roots):
+    """Generic function to find package_data.
+    All of the files under each of the `roots` will be declared as package
+    data for package `pkg`.
+    """
+    data = []
+    for root in roots:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+
 setup(
     name='modwsgideploy',
     version=version,
@@ -34,6 +50,11 @@ setup(
                      ],
     package_dir={'modwsgideploy':
                          'modwsgideploy'},
+    #package_data=add_to_package_data("{{cookiecutter.folder_name}}")
+    #This causes to be included in bdist, vs Manifest.in only gets included in sdist
+    # data_files=[('{{cookiecutter.folder_name}}', ['*']),
+    #               ('config', ['cfg/data.cfg']),
+    #               ('/etc/init.d', ['init-script'])],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
